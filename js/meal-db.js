694fed8b1eff4d11d6c87b3a48908b5c6029459c
Mahin05 <mahin.islam05@gmail.com>
2022-02-27 22:14:19 +0600

    resolved commit

--- js/meal-db.js
+++ js/meal-db.js
@@ -61,39 +61,39 @@ const displaySearchOutput = meals => {
         `;
         searchResult.appendChild(div);
         toggleSpinner('none');
-        toggleSearchResult('block');
+        toggleSearchResult('flex');
 
     })
 }
-// const loadMeals = searchText => {
-//     url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => displayMeals(data.meals))
-// }
+const loadMeals = searchText => {
+    url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
 
-// const displayMeals = meals => {
-//     const conatiner = document.getElementById('search-result');
-//     conatiner.textContent = '';
-//     meals.forEach(meal => {
-//         const div = document.createElement('div');
-//         div.classList.add('col');
-//         div.innerHTML = `
-// <div class="col">
-// <div  onclick="loadMealDetail(${meal.idMeal})" class="col">
-// <div class="card">
-//     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//         <h5 class="card-title">${meal.strMeal}</h5>
-//         <p class="card-text">${meal.strInstructions.slice(0, 300)}</p>
-//     </div>
-// </div>
-// </div>
-//     `
-//         conatiner.appendChild(div);
-//     })
-// }
-// loadMeals('a');
+const displayMeals = meals => {
+    const conatiner = document.getElementById('search-result');
+    conatiner.textContent = '';
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+<div class="col">
+<div  onclick="loadMealDetail(${meal.idMeal})" class="col">
+<div class="card">
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 300)}</p>
+    </div>
+</div>
+</div>
+    `
+        conatiner.appendChild(div);
+    })
+}
+loadMeals('a');
 
 const loadMealDetail = id => {
     // console.log(id);
